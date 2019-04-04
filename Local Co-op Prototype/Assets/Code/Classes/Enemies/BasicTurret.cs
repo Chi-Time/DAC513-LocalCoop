bc9a6b4c2ea2ab7562392b64fc2fd66e2f29b888
James Johnson <music1384@yahoo.co.uk>
2019-04-05 00:41:59 +0100

    Add Turret enemy

---
+++ Local Co-op Prototype/Assets/Code/Classes/Enemies/BasicTurret.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using Assets.Code.Classes.Bullets;
+
+namespace Assets.Code.Classes.Enemies
+{
+    [RequireComponent (typeof (Enemy))]
+    class BasicTurret : MonoBehaviour
+    {
+        [Tooltip ("The amount of damage each bullet does.")]
+        [SerializeField] private int _Damage = 2;
+        [Tooltip ("How long each bullet will stay on screen for.")]
+        [SerializeField] private float _LifeTime = 5;
+        [Tooltip ("The speed of each bullet.")]
+        [SerializeField] private float _BulletSpeed = 10f;
+        [Tooltip ("How many bullets should the turret fire?")]
+        [SerializeField] private int _ShotAmount = 5;
+        [Tooltip ("The rate at which the turret will shoot bullets.")]
+        [SerializeField] private float _FireRate = 0.0f;
+        [Tooltip ("The bullets that the turret will fire.")]
+        [SerializeField] private GameObject _BulletPrefab = null;
+
+        private float _Timer = 0.0f;
+        private Transform _Transform = null;
+
+        private void Awake ()
+        {
+            _Transform = GetComponent<Transform> ();
+        }
+
+        private void Update ()
+        {
+            CalculateTimer ();
+
+            if (CanFire ())
+                Shoot ();
+        }
+
+        private void CalculateTimer ()
+        {
+            _Timer += Time.deltaTime;
+        }
+
+        private bool CanFire ()
+        {
+            if (_Timer >= _FireRate)
+            {
+                _Timer = 0.0f;
+                return true;
+            }
+
+            return false;
+        }
+        
+        private void Shoot ()
+        {
+            float angleStep = 360 / _ShotAmount;
+            float currentAngle = 0.0f;
+
+            for (int i = 0; i < _ShotAmount; i++)
+            {
+                var bulletObj = Instantiate (_BulletPrefab, _Transform.position, _Transform.rotation);
+
+                currentAngle += angleStep;
+                bulletObj.transform.rotation = Quaternion.Euler (90f, currentAngle, 0f);
+
+                var bullet = bulletObj.GetComponent<Bullet> ();
+                bullet.Constructor (_Damage, _BulletSpeed, _LifeTime);
+            }
+        }
+    }
+}
---
+++ Local Co-op Prototype/Assets/Code/Classes/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using Assets.Code.Classes.Utilities;
+
+namespace Assets.Code.Classes
+{
+    class Enemy : MonoBehaviour
+    {
+        [Tooltip ("The amount of damage the enemy does upon colliding.")]
+        [SerializeField] private int _Damage = 2;
+        [Tooltip ("How much health does this enemy have?")]
+        [SerializeField] private int _Health = 100;
+        [Tooltip ("Ho many points does this enemy give off after being killed?")]
+        [SerializeField] private int _Value = 10;
+        [Tooltip ("Can this enemy be flown into by the player?")]
+        [SerializeField] private bool _CanBeCollided = false;
+        [Tooltip ("Should the enemy die upon being collided with?")]
+        [SerializeField] private bool _ShouldDieUponCollision = true;
+
+        private void OnEnable ()
+        {
+            LevelSignals.OnEntityHit += OnEntityHit;
+        }
+
+        private void OnEntityHit (int damage, GameObject entity)
+        {
+            if (entity == this.gameObject)
+            {
+                _Health -= damage;
+
+                if (_Health <= 0)
+                    Kill ();
+            }
+        }
+
+        private void Kill ()
+        {
+            Destroy (this.gameObject);
+
+            LevelSignals.IncreaseScore (_Value);
+        }
+
+        private void OnDisable ()
+        {
+            LevelSignals.OnEntityHit -= OnEntityHit;
+        }
+
+        private void OnTriggerEnter (Collider other)
+        {
+            if (_CanBeCollided && other.CompareTag ("Player"))
+            {
+                if (_ShouldDieUponCollision)
+                {
+                    Kill ();
+                }
+
+                LevelSignals.HitEntity (_Damage, other.gameObject);
+            }
+        }
+    }
+}
